a713c4e4991afc77bbb6ea1a965e39697276964a
Anh Huy <nguyen.a.huy03217@gmail.com>
2020-12-13 21:05:05 +0700

    add debug

--- send_mail.php
+++ send_mail.php
@@ -150,13 +150,14 @@
             } else if($purpose == 3) {  //join class request
                 if (!empty($_POST['class-code'])) {
                     $mail = new PHPMailer(true);
-                    $mail->CharSet = 'UTF-8';
-                    $mail->isSMTP();                                            // Send using SMTP
+                    $mail->isSMTP();
+                    $mail->CharSet = 'UTF-8';                     // Send using SMTP
                     $mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
                     $mail->SMTPAuth = true;                                  // Enable SMTP authentication
                     $mail->Username = 'server.classroom3366@gmail.com';                     // SMTP username
                     $mail->Password = 'Huy12345678';                               // SMTP password
-                    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                    $mail->SMTPSecure = 'tls';
+                    $mail->SMTPDebug = 0;   // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                     $mail->Port = 587;
 
                     try {
@@ -262,7 +263,8 @@
                         $mail->SMTPAuth = true;                                   // Enable SMTP authentication
                         $mail->Username = 'server.classroom3366@gmail.com';                     // SMTP username
                         $mail->Password = 'Huy12345678';                               // SMTP password
-                        $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                        $mail->SMTPSecure = 'tls';
+                        $mail->SMTPDebug = 0;   // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                         $mail->Port = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
                         //Recipients
@@ -301,7 +303,8 @@
                      $mail->SMTPAuth = true;                                   // Enable SMTP authentication
                      $mail->Username = 'server.classroom3366@gmail.com';                     // SMTP username
                      $mail->Password = 'Huy12345678';                               // SMTP password
-                     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                     $mail->SMTPSecure = 'tls';
+                     $mail->SMTPDebug = 0;// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                      $mail->Port = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
                      //Recipients
